9d5cf8889f1ce5892d45dc3e38ff60cc8134305e
lijiaju <2493225721@qq.com>
2026-09-17 20:17:21 +0800

    chore: 初始化 Spring Boot 工程骨架

---
+++ src/main/java/org/xy/XyApplication.java
@@ -0,0 +1,13 @@
+package org.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyApplication.class, args);
+    }
+
+}
